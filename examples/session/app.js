8210f6cc01dc7b102defe824246468a57a4bf7d5
casperchen(陈映平) <chyingp@gmail.com>
2016-08-15 01:01:34 +0800

    添加注释

--- examples/session/app.js
+++ examples/session/app.js
@@ -62,13 +62,16 @@ app.post('/login', function(req, res, next){
 });
 
 app.get('/logout', function(req, res, next){
+	// 备注：这里用的 session-file-store 在destroy 方法里，并没有销毁cookie
+	// 所以客户端的 cookie 还是存在，导致的问题 --> 退出登陆后，服务端检测到cookie
+	// 然后去查找对应的 session 文件，报错
+	// session-file-store 本身的bug
 	req.session.destroy(function(err) {
 		if(err){
 			res.json({ret_code: 2});
 			return;
 		}
 		
-		// res.json({ret_code: 0});
 		res.redirect('/');
 	});
 });
